4d14fc999f2adc3989123e1d13baa2eeaf1ce055
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2025-08-14 13:10:37 +0530

    try to remove users2 table

--- run_liquibase.sh
+++ run_liquibase.sh
@@ -2,9 +2,8 @@
 set -e  # exit if any command fails
 
 liquibase \
-            --url="jdbc:postgresql://postgres:5432/cruddb" \
-            --username=postgres \
-            --password=7702 \
-            --changeLogFile=db/changelog/db.changelog-master.xml \
-            --classpath=lib/postgresql-42.7.3.jar \
-            update
+  --url="jdbc:postgresql://localhost:5432/cruddb" \
+  --username="postgres" \
+  --password="7702" \
+  --changeLogFile="db/changelog/db.changelog-master.sql" \
+  update
