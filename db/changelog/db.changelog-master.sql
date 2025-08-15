d88d2cb2b63615ffbc7a511cdbfbbb14c470fa5f
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2025-08-15 12:13:47 +0530

    try to add users3 table with id 9

--- db/changelog/db.changelog-master.sql
+++ db/changelog/db.changelog-master.sql
@@ -26,3 +26,10 @@ CREATE TABLE users3 (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+--changeset me:9
+CREATE TABLE users3 (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
