9d1a87bf282d6c005835cf53fa0523f6e7610762
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2025-08-15 13:14:15 +0530

    try to reset

--- run_liquibase.sh
+++ run_liquibase.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e  # exit if any command fails
 
-liquibase --url="jdbc:postgresql://localhost:5432/cruddb" \
-  --username="postgres" \
-  --password="7702" \
-  --changeLogFile="db/changelog/db.changelog-master.sql" \
-  clearCheckSums && \
-liquibase --url="jdbc:postgresql://localhost:5432/cruddb" \
+liquibase \
+  --url="jdbc:postgresql://localhost:5432/cruddb" \
   --username="postgres" \
   --password="7702" \
   --changeLogFile="db/changelog/db.changelog-master.sql" \
