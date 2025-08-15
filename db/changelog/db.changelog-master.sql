4bc261f5f447bfae57950ec292b32cc47df9c986
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2025-08-15 12:20:23 +0530

    try to add users3 table with id 9

--- db/changelog/db.changelog-master.sql
+++ db/changelog/db.changelog-master.sql
@@ -1,33 +1,26 @@
 --liquibase formatted sql
 
---changeset me:4
+--changeset me:1
+DROP TABLE IF EXISTS users3;
+DROP TABLE IF EXISTS users2;
+DROP TABLE IF EXISTS users1;
+DROP TABLE IF EXISTS users;
+
+--changeset me:2
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
---changeset me:5
-DROP TABLE IF EXISTS users1;
-
---changeset me:6
+--changeset me:3
 CREATE TABLE users2 (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
---changeset me:7
-DROP TABLE IF EXISTS users2;
-
---changeset me:8
-CREATE TABLE users3 (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
---changeset me:9
+--changeset me:4
 CREATE TABLE users3 (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
