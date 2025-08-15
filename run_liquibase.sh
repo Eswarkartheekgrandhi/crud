50273deca667b5f1850cd9d7ea731b4af54f1b03
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2025-08-15 12:54:37 +0530

    try to add users3 table with id 9

--- run_liquibase.sh
+++ run_liquibase.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e  # exit if any command fails
 
-liquibase \
+./liquibase \
   --url="jdbc:postgresql://localhost:5432/cruddb" \
-  --username="postgres" \
-  --password="7702" \
-  --changeLogFile="db/changelog/db.changelog-master.sql" \
-  update
+  --username=postgres \
+  --password=your_password \
+  --changeLogFile=db/changelog/db.changelog-master.sql \
+  --classpath=lib/postgresql-42.7.3.jar \
+  clearCheckSums
+
